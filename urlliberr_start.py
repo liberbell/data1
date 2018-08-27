b05a7932fab0d5b720a2e6eaaafad4d5c9f34390
liberbell <liberbell@gmail.com>
2018-08-28 08:48:21 +0900

    err4

--- urlliberr_start.py
+++ urlliberr_start.py
@@ -3,7 +3,7 @@
 # TODO: import the request, error, and status modules
 import urllib.request
 from http import HTTPStatus
-from urllib.error import HTTPError
+from urllib.error import HTTPError, URLError
 
 
 def main():
@@ -19,6 +19,8 @@ def main():
             print(result.read().decode('utf-8'))
     except HTTPError as err:
         print('Error: {0}'.format(err.code))
+    except URLError as err:
+        print('That server is bunk')
 
 if __name__ == "__main__":
     main()
