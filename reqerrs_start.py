bf950315e3b8f7e3a3a2c6e58cc37a4465bfc0c9
liberbell <liberbell@gmail.com>
2018-08-29 08:52:25 +0900

    reqerr2

--- reqerrs_start.py
+++ reqerrs_start.py
@@ -1,14 +1,19 @@
 # using the requests library to access internet data
 
 import requests
+from requests import HTTPError
 
 def main():
     # Use requests to issue a standard HTTP GET request
-    url = "http://httpbin.org/status/404"
-    result = requests.get(url)
-    printResults(result)
+    try:
+        url = "http://httpbin.org/status/404"
+        result = requests.get(url)
+        result.raise_for_status()
+        printResults(result)
+    except HTTPError as err:
+        print('Error: {0}'.format(err))
 
-    
+
 
 def printResults(resData):
     print("Result code: {0}".format(resData.status_code))
