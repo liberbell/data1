c9c23c5ba555e65fa8c55fa14410507803aa17a6
liberbell <liberbell@gmail.com>
2018-08-28 09:05:37 +0900

    req1

---
+++ requests_start.py
@@ -0,0 +1,33 @@
+# using the requests library to access internet data
+
+#import the requests library
+import requests
+
+def main():
+    # TODO: Use requests to issue a standard HTTP GET request
+    url = 'http://httpbin.org/xml'
+    result = requests.get(url)
+    printResults(result)
+
+
+    # TODO: Send some parameters to the URL via a GET request
+    # Note that requests handles this for you, no manual encoding
+
+
+    # TODO: Pass a custom header to the server
+
+
+
+def printResults(resData):
+    print("Result code: {0}".format(resData.status_code))
+    print("\n")
+
+    print("Headers: ----------------------")
+    print(resData.headers)
+    print("\n")
+
+    print("Returned data: ----------------------")
+    print(resData.content)
+
+if __name__ == "__main__":
+    main()
