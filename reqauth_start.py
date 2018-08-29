d8be6278d91b328b6f6efd92719cdda4efc9cf52
liberbell <liberbell@gmail.com>
2018-08-29 09:07:58 +0900

    auth2

--- reqauth_start.py
+++ reqauth_start.py
@@ -9,8 +9,10 @@ def main():
     url = "http://httpbin.org/basic-auth/JoeMarini/MySecWord"
 
     # TODO: Create a credentials object using HTTPBasicAuth
+    myCreds = HTTPBasicAuth('JoeMarini', 'MySecWord')
 
     # TODO: Issue the request with the authentication credentials
+    result = requests.get(url, auth=myCreds)
 
     printResults(result)
 
