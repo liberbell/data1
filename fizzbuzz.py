10f850f3875a89f8f2a64f37caf07b3c5298b851
liberbell <liberbell@gmail.com>
2018-08-31 15:54:53 +0900

    fizz2

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+def fizzbuzz(num):
+    if num % 15 == 0:
+        ret = 'fizzbuzz'
+    elif num % 3 == 0:
+        ret = 'fizz'
+    elif num % 5 == 0:
+        ret = 'buzz'
+    else:
+        ret = str(num)
+    return ret
+
+for num in range(1,101):
+    print(fizzbuzz(num))
