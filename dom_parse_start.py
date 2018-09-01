34c0efb5a4f37ecf36d2a8c3da822efc249c5aa7
liberbell <liberbell@gmail.com>
2018-09-01 16:31:52 +0900

    dom3

--- dom_parse_start.py
+++ dom_parse_start.py
@@ -12,9 +12,9 @@ def main():
     # TODO: parse the returned content into a DOM structure
     domtree = xml.dom.minidom.parseString(result.text)
     rootnode = domtree.documetElement
-
     # TODO: display some information about the content
     print('The root element is {0}'.format(rootnode.nodeName))
+    print('Title: {0}'.format(rootnode.getAttribute('title')))
 
     # manipulate the XML content in memory
     # TODO: create a new item tag
