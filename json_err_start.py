0d6c85b0a212d9942dab0f613a0f625400fe614a
liberbell <liberbell@gmail.com>
2018-09-01 16:35:21 +0900

    json10

--- json_err_start.py
+++ json_err_start.py
@@ -13,7 +13,7 @@ def main():
                 "Thousand Island Dressing",
                 "Sauerkraut",
                 "Pickles"
-            ]
+            ],
             "price" : 8.99
         }'''
 
